480ac085d3fc0d3594d0c5ba7b69199b546b3198
Laureano Cejas <laurecejas2510@gmail.com>
2025-10-17 11:18:17 -0300

    Primer commit: ejemplo de lectura simple desde BD

---
+++ WindformApp/FrmPokemons.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindformApp
+{
+    public partial class FrmPokemons : Form
+    {
+        public FrmPokemons()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPokemons_Load(object sender, EventArgs e)
+        {
+            PokemonNegocios negocio=new PokemonNegocios();
+            dgvPokemons.DataSource = negocio.Listar();
+        }
+    }
+}
---
+++ WindformApp/PokemonNegocios.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WindformApp
+{
+    internal class PokemonNegocios
+    {
+        public List<Pokemons>Listar()
+        {
+            List<Pokemons>lista=new List<Pokemons>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server=DESKTOP-PHQLRTP;database=POKEDEX_DB;integrated security=true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "Select Numero,Nombre,Descripcion From POKEMONS";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector= comando.ExecuteReader();
+                
+                while(lector.Read())
+                {
+                    Pokemons aux = new Pokemons();
+                    aux.Numero = lector.GetInt32(0);
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+
+                    lista.Add(aux);
+
+                }
+                conexion.Close();
+                return lista;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+    }
+}
